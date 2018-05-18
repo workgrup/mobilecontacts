ba4eec48c74674a2f573a5ab209070a9c1658f29
Camilo <camilobar20@hotmail.com>
2018-05-18 14:35:06 -0500

    Add files via upload

---
+++ js/Contactos.js
@@ -0,0 +1,29 @@
+$('#consultarContactos').click(function(){
+	$.mobile.loading('Show',{
+		text : 'Cargando.....',
+		textVisible : true;
+		theme : 'b',
+		html : ''
+	});
+
+	var camposABUSCAR = ['displayName'];
+
+
+	navigator.contacts.find(camposABUSCAR, contactosExito, contactosError);
+});
+
+function contactosExito(){
+	for(var i =0; izcontactos.length; i++){
+		if (contactos[i].displayName) {
+			$('#listaContactos').append('<li>' +contactos[i].displayName+'</li>');
+
+		} 
+		
+	}
+	$('#listaContactos').listview('refresh');
+	$.mobile.loading('hide');
+}
+
+function contactosError(message){
+	alert('Error al cargar contactos: ' +message);
+}
